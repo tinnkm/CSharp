28d6494844dd7c85dfaa7724d1c664f1f7db552f
tinnkm <husong185@163.com>
2016-05-31 16:40:19 +0800

    first commit

---
+++ CSharpLearning01/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CSharpLearning01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filetree = new System.Windows.Forms.TreeView();
+            this.open = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.showdetail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // filetree
+            // 
+            this.filetree.Location = new System.Drawing.Point(12, 60);
+            this.filetree.Name = "filetree";
+            this.filetree.Size = new System.Drawing.Size(195, 338);
+            this.filetree.TabIndex = 0;
+            this.filetree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.filetree_AfterSelect);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(354, 13);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 1;
+            this.open.Text = "导入目录";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // showdetail
+            // 
+            this.showdetail.Location = new System.Drawing.Point(242, 60);
+            this.showdetail.Multiline = true;
+            this.showdetail.Name = "showdetail";
+            this.showdetail.Size = new System.Drawing.Size(201, 338);
+            this.showdetail.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "=>";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 410);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showdetail);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.filetree);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView filetree;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox showdetail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ CSharpLearning01/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpLearning01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                TreeNode subnode = filetree.Nodes.Add(textBox1.Text.Substring(textBox1.Text.LastIndexOf(@"\") + 1));
+                GetTree(subnode, textBox1.Text);
+            }
+        }
+
+        private void GetTree(TreeNode nodes,string locattion)
+        {
+            string[] directorys = Directory.GetDirectories(locattion);
+            if (directorys.Length > 0) { 
+                foreach (var dir in directorys)
+                {
+                    TreeNode subnode = nodes.Nodes.Add(dir.Substring(dir.LastIndexOf(@"\") + 1));
+                    GetTree(subnode, dir);
+                }
+            }
+            
+            string[] files = Directory.GetFiles(locattion);
+            if (files.Length > 0) { 
+                foreach (var file in files)
+                {
+                    nodes.Nodes.Add(file.Substring(file.LastIndexOf(@"\") + 1));
+                }
+            }
+        }
+
+       
+    }
+}
